17359258ab6685a085ec494e8bfb900dbf1ee626
dimpleddhok <dimple.dhok@konverge.ai>
2022-06-08 11:39:42 +0530

    add flask app

---
+++ create_tables.py
@@ -0,0 +1,16 @@
+from multiprocessing.dummy import connection
+import sqlite3
+from venv import create
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table="CREATE TABLE IF NOT EXISTS  users (id INTEGER PRIMARY KEY, username text ,password text)"
+cursor.execute(create_table)
+
+create_table="CREATE TABLE IF NOT EXISTS  items (name text,price real)"
+cursor.execute(create_table)
+
+cursor.execute("INSERT INTO items VALUES ('test',10.99)")
+connection.commit()
+connection.close()
